d19dc4a090bdb8974eb59e9d1273de350a1c10e0
DavidMukhiya <sherlock.41v94@gmail.com>
2022-05-18 19:58:52 -0700

    Header Added

--- src/App.js
+++ src/App.js
@@ -1,21 +1,12 @@
 import './App.css';
-import { Container, Navbar, NavbarBrand } from 'reactstrap';
-import hymnsLogo from './app/assets/img/hymns.png';
-//import HymnsCard from './features/hymns/HymnsCard';
-//import { CAMPSITES } from './app/shared/CAMPSITES';
 import HymnsList from './features/hymns/HymnsList';
+import Header from './components/Header';
 
 
 function App() {
   return (
     <div className="App">
-      <Navbar dark color='primary' sticky='top' expand='md'>
-        <Container>
-          <NavbarBrand>
-            <img src={hymnsLogo} alt='Nucamp logo' />
-          </NavbarBrand>
-        </Container>
-      </Navbar>
+      <Header />
       {/* <HymnsCard campsite={CAMPSITES[0]} /> */}
       <HymnsList />
     </div>
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,15 @@
+import { Navbar, NavbarBrand } from 'reactstrap';
+import hymnsLogo from '../app/assets/img/hymns.png';
 
-
-const Header = ()=>{
-    return(
-        <header className="App-header">
-            <h1>Nepali Hymns</h1>
-        </header>
-    )
+const Header = () => {
+    return (
+        <Navbar dark color='primary' sticky='top' expand='md'>
+            <NavbarBrand>
+                <img src={hymnsLogo} alt='Nucamp logo' />
+                <span><h1>Nepali Hymns</h1></span>
+            </NavbarBrand>
+        </Navbar>
+    );
 }
 
 export default Header;
